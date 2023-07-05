2e6196184009d03db18282693b9ce3927d52a9b4
Vincent <vincent14@student.umn.ac.id>
2023-07-06 00:18:51 +0700

    adding modal, state management using zustand

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
-import "./globals.css";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+
+import { ModalProvider } from "@/providers/modal-provider";
+
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +16,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ModalProvider />
+          {children}
+          </body>
       </html>
     </ClerkProvider>
   );

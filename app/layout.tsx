1e82bb342d8a4fe1950bef842347eb109146afb0
Vincent <vincent14@student.umn.ac.id>
2023-07-06 09:14:48 +0700

    setup planetscale db using prisma

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
+
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
---
+++ lib/prismadb.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@prisma/client";
+
+// adding prisma to globalThis
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+const prismadb = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
+
+export default prismadb;

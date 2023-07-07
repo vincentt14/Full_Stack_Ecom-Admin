37c2dcbce0f4339cd9de624bd88d85a76d4ceecb
Vincent <vincent14@student.umn.ac.id>
2023-07-07 23:01:21 +0700

    setup settings page root and validation

---
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import * as z from "zod";
+import { Store } from "@prisma/client";
+import { Trash } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+
+import { Heading } from "@/components/ui/heading";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+
+interface SettingsFormProps {
+  initialData: Store;
+}
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
+
+type SettingsFormValues = z.infer<typeof formSchema>;
+
+export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const form = useForm<SettingsFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
+
+  const onSubmit = async (data: SettingsFormValues) => {
+    console.log(data);
+  };
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading title="Settings" description="Manage store preferences" />
+        <Button variant="destructive" size="sm" onClick={() => {}}>
+          <Trash className="h-4 w-4" />
+        </Button>
+      </div>
+      <Separator />
+      
+    </>
+  );
+};

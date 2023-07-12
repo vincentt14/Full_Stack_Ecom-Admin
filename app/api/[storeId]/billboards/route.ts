330745c49658015a6b3aacf6eea3c5e84281361e
Vincent <vincent14@student.umn.ac.id>
2023-07-12 17:59:51 +0700

    creating billboard store api

---
+++ app/api/[storeId]/billboards/route.ts
@@ -0,0 +1,36 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request, { params }: { params: { storeId: string } }) {
+  try {
+    const body = await req.json();
+
+    const { label, imageUrl } = body;
+
+    if (!label) {
+      return new NextResponse("Label is required", { status: 400 });
+    }
+
+    if (!imageUrl) {
+      return new NextResponse("Image URL is required", { status: 400 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+
+    const billboard = await prismadb.billboard.create({
+      data: {
+        label,
+        imageUrl,
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(billboard);
+  } catch (error) {
+    console.log("[BILLBOARDS_POSTS]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
